21cffa2e98d8f5082a84fa7bcd496e627529f80d
AndriyKozh <max190716@ukr.net>
2022-09-03 11:48:40 +0300

    2

--- react-projects/05-react-green-button/script.js
+++ react-projects/05-react-green-button/script.js
@@ -1,15 +1,20 @@
-const App = () => {
-  const [buttonText, setButtontext] = React.useState("Click me");
+const App = ({ initialButtonText, initialClassesList }) => {
+  const [buttonText, setButtontext] = React.useState(initialButtonText);
+
+  const [classesList, setClassesList] = React.useState(initialClassesList);
 
   const onButtonClick = () => {
     setButtontext("Hello frome React");
+    setClassesList("green-btn");
   };
   return (
     <div className="app">
-      <button onClick={onButtonClick}>{buttonText}</button>
+      <button className={classesList} onClick={onButtonClick}>
+        {buttonText}
+      </button>
     </div>
   );
 };
 const container = document.getElementById("app");
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(<App initialButtonText="Click me" initialClassesList="" />);
